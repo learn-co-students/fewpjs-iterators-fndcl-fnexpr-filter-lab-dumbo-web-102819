1be48305619207673ddeb9af0aa78913bf6731ea
Frank Gerold <FrankETSGerold@gmail.com>
2019-12-15 11:18:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching (drivers, string) {
+
+    return drivers.filter( driver =>
+      driver.toLowerCase() === string.toLowerCase() )}
+
+//   return drivers.filter((driver) => {
+//     driver.toUpperCase() === string.toUpperCase()
+//   })
+// }
+
+  // // let list = drivers.map((a) => a.toUpperCase());
+  // let str = string.toUpperCase;
+  // let final = [];
+  // for (let item of ele) {
+  //   final.push(string[list.indexOf(ele)]
+  // }
+  // return final;
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(driver => driver.slice(0, string.length) === string)
+}
+
+function matchName (drivers, string) {
+  return drivers.filter((driver) => driver.name === string)
+}
